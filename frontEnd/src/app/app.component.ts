dafcd3a1b04e4313e4e95f5b51fb90f16f5715ae
Alex Paredes <aapa96@gmail.com>
2017-05-03 13:09:26 -0500

    Creacion de servicio prueba OnInit

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {User} from '../Models/users';
+import {UserService} from '../services/users.service';
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers:[UserService]
   //styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 	public title = 'app works!';
 	public user: User;
 	public identity = true;
 	public token; 
 
-	constructor(){
+	constructor(
+		private _userService:UserService
+		){
 		this.user = new User ('','','','','','','','ROLE_USER');
 	}
 
-
+	ngOnInit(){
+		var texto = this._userService.signup();
+		console.log(texto);
+	}
 	public onSubmit(){
 		console.log(this.user);
 	}
