7c02d12ffdf9807e035b95f77b2b6388190ceb11
Alex Paredes <aapa96@gmail.com>
2017-05-03 01:26:08 -0500

    Controlador usuario completado + token + middleware

--- apiRest/routes/users.js
+++ apiRest/routes/users.js
@@ -2,17 +2,14 @@
 
 var express = require('express');
 var UserController = require('../controllers/users');
-
+var md_auth = require('../middlewares/authenticated');
 
 
 var api = express.Router();
 
-api.get('/ControladorU',UserController.pruebas);
+api.get('/ControladorU',md_auth.ensureAuth,UserController.pruebas);
 api.post('/register',UserController.saveUser);
 api.post('/loginUser',UserController.loginUser);
-
-
-
-
+api.put('/updateUser/:id',md_auth.ensureAuth,UserController.updateUser);
 
 module.exports=api;
